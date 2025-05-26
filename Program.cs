4dd7d44cfc5d0605675971b75a1d1542a890e596
Antonio Eduardo Mendez <edumendez120403@gmail.com>
2025-05-25 21:29:31 -0300

    primer punto

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+/*int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("Valor de a:"+a);
+Console.WriteLine("Valor de b:"+b);*/
+
+
+/*EJERCICIO 1:*/
+
+Console.WriteLine("Escriba un numero:");
+string numero = Console.ReadLine();
+int num = 0;
+bool esNumero = int.TryParse(numero, out num);
+if (esNumero)
+{
+    if (num > 0)
+    {
+        char[] arr = numero.ToCharArray();
+        Array.Reverse(arr);
+        string numeroInvertido = new string(arr);
+        Console.WriteLine("El numero invertido es: " + numeroInvertido);
+    }
+    else
+    {
+        Console.WriteLine("El numero no es mayor a 0");
+    }
+}
+else
+{
+    Console.WriteLine("El texto ingresado no es un numero valido");
+}
